1be25a3f4698e13e1008b483844481d22db4cf04
Joey Poon <joey@joeypoon.com>
2015-05-28 15:13:08 -0500

    update bottom post links w/ dynamic content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,18 @@
 class PagesController < ApplicationController
+  include PagesHelper
+
+  # before_actions do
+  #   @posts = ["ANOTHER COOL POST", "POST TITLE", "HERE'S ANOTHER LONGER POST TITLE", "OH COOL A POST"]
+  # end
 
   def most_popular_post
     @title = "so engaging, much fantastic".upcase
+    @content = content[@title.parameterize]
   end
 
   def post
-    @title = params[:post_name]
+    @title = params[:post_name].upcase
+    @content = content[@title.parameterize]
   end
 
 end
